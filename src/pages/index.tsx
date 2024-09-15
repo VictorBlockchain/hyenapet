ea02012680401ef1eb4f4b5efc8d702534c7541d
victorblockchain <93537143+VictorBlockchain@users.noreply.github.com>
2024-09-15 10:28:27 -0500

    added loading gif

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,17 @@ import type { GetServerSideProps, NextPage } from 'next'
 import dynamic from 'next/dynamic'
 import { useState } from "react";
 import Layout from '../components/Layout';
-const Start = dynamic(() => import("../components/Start"));
-
+// const Start = dynamic(() => import("../components/Start"));
+const LoadingComponent = () => {
+	return (
+	  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+		<Image src="/img/loading.gif" alt="Loading..." width={100} height={100} />
+	  </div>
+	);
+  };
+const Start = dynamic(() => import('../components/Start'), {
+	loading: LoadingComponent
+  });
 export default function Home() {
 
 	return (
