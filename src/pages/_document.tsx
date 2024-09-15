26e285365a79b9f6443367a1acb96a6846ee936f
victorblockchain <93537143+VictorBlockchain@users.noreply.github.com>
2024-09-15 15:21:57 -0500

    latest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html className="no-js" lang="en">
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+		<Html lang="en">
         <Head>
-          <meta charSet="UTF-8" />
-		  <link rel="manifest" href="/manifest.json" />
+		<meta charSet="UTF-8" />
+
+			{/* <title>Hyena Pets | By XFT.red</title>
+				<meta
+					name="description"
+					content="Magical, Mythical, Battle Ready, Hyena's As Pets"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/> */}
+				{/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+				<link rel="manifest" href="/manifest.json" />
 				<link rel="icon" href="/favicon.ico" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Alkatra&family=Comfortaa:wght@300;400;500;600&family=Delicious+Handrawn&family=Josefin+Sans:ital,wght@0,300;0,500;0,600;1,500;1,600;1,700&family=Vina+Sans&display=swap"
@@ -30,17 +41,11 @@ class MyDocument extends Document {
                 `,
               }}
             ></script>
-        </Head>
-        <body>
-        {/* <div className="cs-preloader cs-center">
-          <div className="cs-preloader_in"></div>
-          <span>Loading</span>
-        </div> */}
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+		</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
-export default MyDocument;
