c3426fd0aa21850c44ac2489f024c01ce5d17460
victorblockchain <93537143+VictorBlockchain@users.noreply.github.com>
2024-09-15 10:38:29 -0500

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-import type { GetServerSideProps, NextPage } from 'next'
 import dynamic from 'next/dynamic'
-import { useState } from "react";
 import Layout from '../components/Layout';
 // const Start = dynamic(() => import("../components/Start"));
 const LoadingComponent = () => {
