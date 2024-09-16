1e379f84d7a8ceb7cb8a46ba3c4cc0c2c2ae4884
victorblockchain <93537143+VictorBlockchain@users.noreply.github.com>
2024-09-16 09:34:06 -0500

    latest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import dynamic from 'next/dynamic'
 import Layout from '../components/Layout';
-// const Start = dynamic(() => import("../components/Start"));
-const LoadingComponent = () => {
-	return (
-	  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-		<Image src="/img/loading.gif" alt="Loading..." width={100} height={100} />
-	  </div>
-	);
-  };
-const Start = dynamic(() => import('../components/Start'), {
-	loading: LoadingComponent
-  });
+const Start = dynamic(() => import("../components/Start"));
+// const LoadingComponent = () => {
+// 	return (
+// 	  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+// 		<Image src="/img/loading.gif" alt="Loading..." width={100} height={100} />
+// 	  </div>
+// 	);
+//   };
+// const Start = dynamic(() => import('../components/Start'), {
+// 	loading: LoadingComponent
+//   });
   const Home = () => {
 	return (
 	  <Layout>
